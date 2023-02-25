a7cf039e7c7ea03fbe5fa1fa20a0df151f12b468
Abhishek Das <abhid14@outlook.com>
2023-02-25 18:30:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!asdadadad')
+})
+app.get('/user/yash', (req, res) => {
+    // data fetch from sql
+
+    res.send('Hello World from yash!')// send 
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
